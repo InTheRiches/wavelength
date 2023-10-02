fec155eca8d3c0ddea3faa2ef4e1461f31977536
InTheRiches <intherichesftk@gmail.com>
2023-10-01 21:35:51 -0400

    Worked new muscle page.

--- pages/demo-muscles/index.js
+++ pages/demo-muscles/index.js
@@ -73,35 +73,52 @@ export default function Layout() {
 
     return (
         <div className={`flex flex-col min-h-screen text-slate-900 dark:text-slate-50 justify-center items-center bg-white dark:bg-neutral-900 ` + openSans.className}>
-            {/*<div className={"absolute top-0 right-0 w-full h-full"}>*/}
-            {/*    <img src={"/landingPageBG.png"} className={"w-full h-full object-cover opacity-50 dark:opacity-20"}></img>*/}
-            {/*</div>*/}
+            <div className={"absolute top-0 right-0 w-full h-full z-10"}>
+                <img src={"/landingPageBG.png"} className={"w-full h-full object-cover opacity-50 dark:opacity-20"}></img>
+            </div>
 
-            <header className={"flex flex-col items-center w-full h-screen"}>
+            <header className={"flex flex-col items-center w-full h-screen z-20"}>
                 <Navigation dark={isDarkMode} setDark={toggleDarkMode}></Navigation>
                 <div className={"flex flex-col h-full w-full p-8"}>
                     <div className={"w-full h-full flex flex-row"}>
                         <div className={"h-full w-1/4"}>
-                            <span className="mb-4 inline-block text-2xl sm:text-4xl font-bold text-slate-900 tracking-tight dark:text-slate-50">Pectoralis Major</span>
-                            <div className={"mb-4 border-cyan-accent border-1 flex flex-col p-3 bg-neutral-500 bg-opacity-5 rounded-md"}>
+                            <span className="mb-6 inline-block text-2xl sm:text-6xl font-bold text-slate-900 tracking-tight dark:text-slate-50">Pectoralis Major</span>
+                            <div className={"mb-6 text-lg flex flex-col p-3 bg-neutral-500 bg-opacity-5 rounded-md border-1 border-cyan-accent"}>
                                 <span>The pectoral muscles are large, fan shaped muscles that reside on either side of the upper chest. Both muscles have two heads, the clavicular, and the sternocostal.</span>
+                            </div>
+                            <span className="mb-4 inline-block text-2xl sm:text-5xl font-bold text-slate-900 tracking-tight dark:text-slate-50">Exercises</span>
+                            <div className={"grid grid-cols-2 gap-4"}>
+                                <div className={"rounded-md p-4 flex flex-col bg-neutral-500 bg-opacity-5 w-full transition-all duration-200 hover:cursor-pointer hover:shadow-button"}>
+                                    <span className="mb-4 inline-block text-2xl sm:text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-50 partial-bottom-border text-center">Bench Press</span>
+                                    <img src={"/exampleBenchPress.png"} className={"w-full h-full aspect-square rounded-md"}></img>
+                                </div>
+
+                                <div className={"rounded-md p-4 flex flex-col bg-neutral-500 bg-opacity-5 w-full transition-all duration-200 hover:cursor-pointer hover:shadow-button"}>
+                                    <span className="mb-4 inline-block text-2xl sm:text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-50 partial-bottom-border text-center">Chest Fly</span>
+                                    <img src={"/exampleChestFly.png"} className={"w-full h-full aspect-square rounded-md"}></img>
+                                </div>
+
+                                <div className={"rounded-md p-4 flex flex-col bg-neutral-500 bg-opacity-5 w-full transition-all duration-200 hover:cursor-pointer hover:shadow-button"}>
+                                    <span className="mb-4 inline-block text-2xl sm:text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-50 partial-bottom-border text-center">Cable Crossover</span>
+                                    <img src={"/exampleCableFly.png"} className={"w-full h-full aspect-square rounded-md"}></img>
+                                </div>
                             </div>
                         </div>
                         <div className={"h-full w-1/2 flex flex-row items-center justify-center p-4"}>
-                                <FrontFullBodySVG big={true}/>
+                                <FrontFullBodySVG highlighted={"pecs"} big={true}/>
                                 <BackFullBodySVG big={true}/>
                         </div>
                         <div className={"h-full w-1/4 p-4"}>
-                            <span className="mb-4 inline-block text-2xl sm:text-4xl font-bold text-slate-900 tracking-tight dark:text-slate-50">Information</span>
-                            <div className={"border-cyan-accent border-1 rounded-md p-4 flex flex-col mb-12"}>
+                            <span className="mb-4 inline-block text-2xl sm:text-5xl font-bold text-slate-900 tracking-tight dark:text-slate-50">Information</span>
+                            <div className={"rounded-md p-4 flex flex-col bg-neutral-500 bg-opacity-5 mb-12"}>
                                 <span className="mb-4 inline-block text-2xl sm:text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-50 partial-bottom-border text-center">Location</span>
-                                <span className={"indent-4"}>
+                                <span className={"indent-4 text-lg"}>
                                     The pectoralis major is a large muscle located in the chest region of the body. It spans from the sternum (breastbone), clavicle (collarbone), and the upper ribs to the humerus (upper arm bone). It covers a significant portion of the anterior chest and is easily visible in well-developed individuals.
                                 </span>
                             </div>
-                            <div className={"border-cyan-accent border-1 rounded-md p-4 flex flex-col"}>
+                            <div className={"rounded-md p-4 flex flex-col bg-neutral-500 bg-opacity-5"}>
                                 <span className="mb-4 inline-block text-2xl sm:text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-50 partial-bottom-border text-center">Function</span>
-                                <span className={"indent-4"}>
+                                <span className={"indent-4 text-lg"}>
                                     The pectoralis major is a muscle in the chest that has several important functions. Its main function is to pull the elbow towards the center of your body. This is the primary movement of things like pushups and bench presses. The pectoralis major plays a crucial role in various upper body movements, and strengthening it can improve strength and overall performance.
                                 </span>
                             </div>
