dec712d071e5f803aada725e5be70eb1ca5ffe87
InTheRiches <intherichesftk@gmail.com>
2023-10-16 22:46:30 -0400

    Added privacy page to footer.

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,11 @@
 export default function Footer({ darkBG = "neutral-900" }) {
-    return <footer className={`bg-cyan-accent rounded-3xl dark:bg-${darkBG} text-slate-50 py-2 px-4 mb-8 shadow-button dark:shadow-none`}>
-        <div className="flex items-center justify-center text-center">
-            <p>&copy; 2023 Wavelength. All rights reserved.</p>
-        </div>
-    </footer>
+    return <>
+        <footer className={`bg-cyan-accent rounded-3xl dark:bg-${darkBG} text-slate-50 py-2 px-4 mb-8 shadow-button dark:shadow-none`}>
+            <div className="flex items-center justify-center text-center">
+                <p>&copy; 2023 Wavelength. All rights reserved.</p>
+                
+                <a href={"/privacy"} className={"hover:underline ml-2 text-cyan-accent"}>Privacy Policy</a>
+            </div>
+        </footer>
+    </>
 }
