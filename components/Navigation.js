6ba1f329c0d56fe7e44408cf665f37400c7b274b
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2023-10-26 12:53:31 -0400

    adjusted margin logic ms

--- components/Navigation.js
+++ components/Navigation.js
@@ -72,7 +72,7 @@ export default function Navigation({dark, setDark}) {
         </div>
       {isOpen ?             
       <div className={"fixed inset-0 z-50 block"}>
-          <div className={"relative flex flex-col bg-white dark:bg-neutral-900 w-80 max-w-[calc(100%-3rem)] p-6 h-screen " + (`mt-[${Math.floor(navHeight)}px]`)}>
+          <div className={"relative flex flex-col bg-white dark:bg-neutral-900 w-80 max-w-[calc(100%-3rem)] p-6 h-screen " + (`[margin-top:${Math.floor(navHeight)}px]`)}>
               <MobileSidebar></MobileSidebar>
           </div>
       </div> 
