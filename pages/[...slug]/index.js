0fc811982c56304aaac45124094bd8921771358b
InTheRiches <intherichesftk@gmail.com>
2023-11-14 20:50:55 -0500

    Improved SEO recognition

--- pages/[...slug]/index.js
+++ pages/[...slug]/index.js
@@ -107,7 +107,7 @@ export default function Page({ headers, title, description="", markdown="", acti
                     <div className="w-full max-w-5xl flex-col">
                         <div className="flex flex-col mb-8">
                             <span className="text-cyan-accent mb-1 text-lg lg:text-xl font-semibold">{location}</span>
-                            <span className="mb-10 inline-block text-4xl xl:text-5xl font-bold text-slate-900 tracking-tight dark:text-slate-50 text-left">{title}</span>
+                            <h1 className="mb-10 inline-block text-4xl xl:text-5xl font-bold text-slate-900 tracking-tight dark:text-slate-50 text-left">{title}</h1>
                             <div className={"border-cyan-accent border-1 flex flex-col p-4 bg-neutral-500 bg-opacity-5 rounded-md text-left sm:text-justify"}>
                                 <span>
                                     <Markdown components={useDescriptionComponents()}>{description}</Markdown>
