ce9d271e7f23bba53464360a1ec12558f03c25f2
InTheRiches <intherichesftk@gmail.com>
2023-11-26 17:00:34 -0500

    Improved definitions

--- components/mdx/Definition.js
+++ components/mdx/Definition.js
@@ -2,10 +2,10 @@ import { Tooltip } from 'react-tooltip'
 
 export default function Definition({ text, definition }) {
     return [
-        <span id={text.replaceAll(" ", "-").toLowerCase()} className="decoration-dashed underline hover:cursor-pointer">
+        <span id={text.replaceAll(" ", "-").toLowerCase()} className="decoration-dashed underline">
             {text}
         </span>,
-        <Tooltip className={"dark:bg-neutral-950 bg-slate-50 dark:text-slate-50 text-neutral-900"} disableStyleInjection={"core"} anchorSelect={"#" + text.replaceAll(" ", "-").toLowerCase()} place="top">
+        <Tooltip className={"dark:bg-neutral-950 bg-white dark:text-slate-50 text-neutral-900"} disableStyleInjection={"core"} anchorSelect={"#" + text.replaceAll(" ", "-").toLowerCase()} place="top">
             {definition}
         </Tooltip>
     ];
