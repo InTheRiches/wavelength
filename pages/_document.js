aa2810195fd70ceaffa0c7035faf82cfa463b311
InTheRiches <intherichesftk@gmail.com>
2023-11-28 20:03:01 -0500

    Added PropellerAds verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+aimport {Html, Head, Main, NextScript} from 'next/document'
 import Script from "next/script";
 import React from "react";
 
@@ -12,6 +12,7 @@ export default function Document() {
                 <meta property="og:image" content="https://wavelength.fit/images/logo.png"/>
                 <meta property="og:url" content="https://www.wavelength.fit"/>
                 <meta property="og:type" content="website"/>
+                <meta name="monetag" content="96f7e6b3959d06fa47617cc95ff3ccf3"></meta>
             </Head>
             <body>
                 <Script strategy={"beforeInteractive"} async src="https://www.googletagmanager.com/gtag/js?id=G-5376MG73KY"></Script>
